fcf94867ec0d44274f8c956edd3de5d79bd194e4
Ritik <160255700+gritik418@users.noreply.github.com>
2024-05-02 21:24:15 +0530

    Profile menu added in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,9 +4,24 @@ import Link from "next/link";
 import { MdSpaceDashboard } from "react-icons/md";
 import { FiLogIn } from "react-icons/fi";
 import { BsPersonPlusFill } from "react-icons/bs";
+import {
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { selectUser } from "@/features/auth/authSlice";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FiLogOut } from "react-icons/fi";
 
 const Navbar = () => {
   const isLoggedIn = true;
+  const user = useSelector(selectUser);
+
+  console.log(user);
 
   return (
     <div className={styles.container}>
@@ -14,7 +29,8 @@ const Navbar = () => {
         <ul className={styles.navItems}>
           <li>
             <Link href="/" className={styles.item}>
-              <MdSpaceDashboard className={styles.icon} /> Dashboard
+              <MdSpaceDashboard className={styles.icon} />{" "}
+              <p className={styles.name}>Dashboard</p>
             </Link>
           </li>
         </ul>
@@ -35,6 +51,25 @@ const Navbar = () => {
               </li>
             </ul>
           </>
+        )}
+
+        {user && user?._id && (
+          <Menu>
+            <MenuButton>
+              <div className={styles.profile}>
+                <Avatar name="Dan Abrahmov" src="/images/avatar.png" />
+                <p>{user.first_name}</p>
+              </div>
+            </MenuButton>
+            <MenuList>
+              <MenuItem className={styles.menuItem}>
+                <RiLockPasswordFill /> Change Password
+              </MenuItem>
+              <MenuItem className={styles.menuItem}>
+                <FiLogOut /> Logout
+              </MenuItem>
+            </MenuList>
+          </Menu>
         )}
       </nav>
     </div>

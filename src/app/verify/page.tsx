ddf19edcc3063ce21edcec003f48a151a91b741d
Ritik <160255700+gritik418@users.noreply.github.com>
2024-05-02 22:21:35 +0530

    Email verification page ui completed

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import { HStack, PinInput, PinInputField } from "@chakra-ui/react";
+import Image from "next/image";
+import { useState } from "react";
 
 const Verify = () => {
-  return <div>Verify</div>;
+  const [otp, setOtp] = useState<string>();
+
+  const handleClick = () => {
+    console.log(otp);
+  };
+
+  return (
+    <div className="flex items-center justify-center px-4 py-6 sm:px-6 sm:py-16 lg:px-8 lg:py-12">
+      <div className="max-w-sm lg:max-w-[60%] xl:max-w-lg">
+        <div className="mt-12 w-full flex items-center justify-center flex-col">
+          <Image
+            src={"/images/verify-img.jpeg"}
+            height={120}
+            width={200}
+            alt="image"
+          />
+          <h1 className="text-3xl mt-4">Email Verification</h1>
+          <p className="text-lg mt-6 text-center font-normal">
+            We emailed you a six-digit code to{" "}
+            <span className="font-bold">gritik418@gmail.com</span>. Enter the
+            code below to confirm your email address.
+          </p>
+
+          <HStack className="mt-10">
+            <PinInput
+              type="number"
+              value={otp}
+              onChange={(text) => setOtp(text)}
+              placeholder=""
+              autoFocus
+              size={"lg"}
+            >
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+
+          <button
+            onClick={handleClick}
+            className="mt-12 transition-all duration-200 ease-in-out bg-[#2680bc] hover:bg-[#5694bc] uppercase text-white text-2xl py-2 px-20 rounded-lg"
+          >
+            Verify
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Verify;
